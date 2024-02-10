b4834aa48eb7927e0d006889c155f8c3dc2c9aba
Ashish Mule <ashishmule707@gmail.com>
2024-02-10 19:30:56 +0530

    added IIFE founction

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Currying function with Example:
 
-// Currying is a technique in functional programming 
-// where a function is transformed into a sequence of functions, 
-// each taking a single argument. This allows for partial application of functions, 
+// Currying is a technique in functional programming
+// where a function is transformed into a sequence of functions,
+// each taking a single argument. This allows for partial application of functions,
 // creating more specialized functions.
 
 // Example in JavaScript:
@@ -20,11 +20,11 @@ curryAdd(2)(3)(5)();
 
 // Palindrome with Example
 
-// A phrase is a palindrome if it reads the same 
+// A phrase is a palindrome if it reads the same
 // forward and backward after converting all uppercase letters into
-// lowercase and removing all non-alphanumeric characters. 
-// For example, the string "level" is a palindrome because it remains the same 
-// when you read it from the beginning to the end and vice versa. 
+// lowercase and removing all non-alphanumeric characters.
+// For example, the string "level" is a palindrome because it remains the same
+// when you read it from the beginning to the end and vice versa.
 
 function isPalindrome(str) {
   // characters and convert to lowercase
@@ -52,10 +52,10 @@ console.log(isPalindrome("hello")); // false
 // Reverse Vowels in JavaScript
 
 function reverseVowels(str) {
-  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
   let left = 0;
   let right = str.length - 1;
-  const arr = str.split('');
+  const arr = str.split("");
 
   while (left < right) {
     if (!vowels.includes(arr[left])) {
@@ -74,10 +74,10 @@ function reverseVowels(str) {
     left++;
     right--;
   }
-  return arr.join('');
+  return arr.join("");
 }
 
-console.log(reverseVowels("hello")) // holle
+console.log(reverseVowels("hello")); // holle
 
 // ********************************************************************************************
 
@@ -100,7 +100,22 @@ const makeStarPattern = () => {
   console.log(str);
 };
 
-makeStarPattern()
+// makeStarPattern()
 
 // ********************************************************************************************
+
+// Immediately Invoked Function Expressions(IIFE)
+
+// An IIFE is a function that is called immediately after it is defined.
+// It's a way to encapsulate code within a function and execute it right away.
+
+(function (date) {
+  console.log("function called", date.getFullYear());
+})(new Date());
+
+// IIFE without any extra brackets
+
+void function (date) {
+  console.log("IIFE without any extra brackets", date.getFullYear());
+}(new Date());
 
